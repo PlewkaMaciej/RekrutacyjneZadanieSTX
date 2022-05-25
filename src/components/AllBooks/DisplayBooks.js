5bc561f9e4803d6cfaa26633ffb4c54b4be2d306
Maciej <plewka-maciej@wp.pl>
2022-05-25 17:04:49 +0200

    add  number of results on the displayed to 15

--- src/components/AllBooks/DisplayBooks.js
+++ src/components/AllBooks/DisplayBooks.js
@@ -1,36 +1,67 @@
-import '../../styles/stylesLibrary.css';
-import DisplaySingleBook from "../SingleBook/DisplaySingleBook"
-import ShowBooksFromYourLibrary from "./ShowBooksFromYourLibrary"
-import { useEffect, useState } from 'react';
+import "../../styles/stylesLibrary.css";
+import DisplaySingleBook from "../SingleBook/DisplaySingleBook";
+import ShowBooksFromYourLibrary from "./ShowBooksFromYourLibrary";
+import { useEffect, useState } from "react";
 function DisplayBooks({ books, setBooks }) {
-    const [addedBooksToYourLibrary, setAddedBooksToYourLibrary] = useState([]);
-    const [inputForSearchBookValue, setInputForSearchBookValue] = useState([]);
-    useEffect(() => {
-    }, [addedBooksToYourLibrary])
-    const inputTextChange=(e)=>{
-      setInputForSearchBookValue(e.target.value.toLowerCase())
+  const [addedBooksToYourLibrary, setAddedBooksToYourLibrary] = useState([]);
+  const [inputForSearchBookValue, setInputForSearchBookValue] = useState([]);
+  const [page_Num, setPage_Num] = useState(15);
+  useEffect(() => {
+    window.addEventListener("scroll", loadBooks);
+  }, [addedBooksToYourLibrary]);
+  const inputTextChange = (e) => {
+    setInputForSearchBookValue(e.target.value.toLowerCase());
+  };
+
+  const loadBooks = () => {
+    if (
+      window.scrollY + window.innerHeight >=
+      document.documentElement.scrollHeight
+    ) {
+      setPage_Num((page_Num) => page_Num + 1);
     }
-    return (
+  };
+  return (
+    <>
+      {books && (
         <>
-            {books && (
-                <>
-                        <div className="booksContainer"> 
-                        <label className="searchForInput" htmlFor="searchBook">Search for a book by title</label>
+          <div className="booksContainer">
+            <label className="searchForInput" htmlFor="searchBook">
+              Search for a book by title
+            </label>
 
-                    <input onChange={inputTextChange} type="text" id="searchBook" 
-                         size="40"></input>
-                        {books.items.map((singleBook, index) => {
-                            return <DisplaySingleBook inputForSearchBookValue={inputForSearchBookValue} setAddedBooksToYourLibrary={setAddedBooksToYourLibrary} addedBooksToYourLibrary={addedBooksToYourLibrary} setBooks={setBooks} books={books} singleBook={singleBook} index={index} key={index} />
-                        })}
-                            <ShowBooksFromYourLibrary addedBooksToYourLibrary={addedBooksToYourLibrary} />
-                        </div>
-                    </>
-            )}
-                </>
-            )
-            }
-            export default DisplayBooks
-
-
-
-
+            <input
+              onChange={inputTextChange}
+              type="text"
+              id="searchBook"
+              size="40"
+            ></input>
+            {books.items.map((singleBook, index) => {
+             if(page_Num>index){
+                return (
+                    <DisplaySingleBook
+                      inputForSearchBookValue={inputForSearchBookValue}
+                      setAddedBooksToYourLibrary={setAddedBooksToYourLibrary}
+                      addedBooksToYourLibrary={addedBooksToYourLibrary}
+                      setBooks={setBooks}
+                      books={books}
+                      singleBook={singleBook}
+                      index={index}
+                      key={index}
+                    />
+                  );
+             }
+             else{
+                 return null
+             }
+            })}
+            <ShowBooksFromYourLibrary
+              addedBooksToYourLibrary={addedBooksToYourLibrary}
+            />
+          </div>
+        </>
+      )}
+    </>
+  );
+}
+export default DisplayBooks;
